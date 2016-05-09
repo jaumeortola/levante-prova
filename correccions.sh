e2fbbfb70dc3168847900b569b4c01642e5c78d9
jaumeortola <jaumeortola@gmail.com>
2016-05-09 18:12:13 +0000

    accentuació valenciana (parcial)

--- correccions.sh
+++ correccions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find val.levante-emv.com/ -type f|xargs sed -i -r 's/èn\b/én/g;'
+find val.levante-emv.com/ -type f|xargs sed -i -r 's/ès\b/és/g;'
 find val.levante-emv.com/ -type f|xargs sed -i 's/\bcomplimiento\b/compliment/g;'
 find val.levante-emv.com/ -type f|xargs sed -i -r 's/([0-9]) %/\1%/g;'
 find val.levante-emv.com/ -type f|xargs sed -i 's/\baçò\b/això/g;'
