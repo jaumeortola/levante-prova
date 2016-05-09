2444efd5f0c0441ecb914f2aa1e3ab0acadd7b43
jaumeortola <jaumeortola@gmail.com>
2016-05-09 17:54:36 +0000

    primeres correccions

--- correccions.sh
+++ correccions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-find . -type f|xargs sed -i 's/\bcomplimiento\b/compliment/g;'
-exit 0
-find . -type f|xargs sed -i -r 's/([0-9]) %/\1%/g;'
-find . -type f|xargs sed -i 's/\baçò\b/això/g;'
-find . -type f|xargs sed -i 's/\baqueixa\b/eixa/g;'
-find . -type f|xargs sed -i 's/\baqueix\b/eixe/g;'
-find . -type f|xargs sed -i 's/\baqueixes\b/eixes/g;'
-find . -type f|xargs sed -i 's/\baqueixos\b/eixos/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\bcomplimiento\b/compliment/g;'
+find val.levante-emv.com/ -type f|xargs sed -i -r 's/([0-9]) %/\1%/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\baçò\b/això/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\baqueixa\b/eixa/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\baqueix\b/eixe/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\baqueixes\b/eixes/g;'
+find val.levante-emv.com/ -type f|xargs sed -i 's/\baqueixos\b/eixos/g;'
 
 
