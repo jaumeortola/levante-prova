aeba880675546fb7c4d8f60cb95aa40ab76d3f1d
jaumeortola <jaumeortola@gmail.com>
2016-05-11 10:12:11 +0000

    update scripts

--- extract-html-files.sh
+++ extract-html-files.sh
@@ -3,10 +3,17 @@ rm text-html/*
 find val.levante-emv.com/ -name \*.html -exec cp --backup=t {} text-html/ \;
 rm ~/github/languagetool-commandline-scripts/original/*
 cp text-html/* ~/github/languagetool-commandline-scripts/original/
-exit 0
 cd ~/github/languagetool-commandline-scripts/
+./lt-check.sh levante onlyconversion
+cat original/*.txt > original/tot
+rm original/*.txt
+rm original/*.html
+rm original/index.html*
+rm original/*~
+mv original/tot original/levante.txt
 ./lt-check.sh levante
-cd -
-echo resultats en ~/github/languagetool-commandline-scripts/resultats
+sudo cp results/levante.txt-lt.html /var/www/html/rr/levante-prova/
+echo resultats en http://www.riuraueditors.cat/levante-prova/levante.txt-lt.html
+
 
 
