9f1bdeb96ab73c35a1ffaca72876870461918a20
jaumeortola <jaumeortola@gmail.com>
2016-05-13 09:30:18 +0000

    update

--- analyze.sh
+++ analyze.sh
@@ -10,5 +10,6 @@ rm original/index.html*
 rm original/*~
 mv original/tot original/levante.txt
 ./lt-check.sh levante
+mv levante.txt-lt.html levante.txt-lt-old.html
 sudo cp results/levante.txt-lt.html /var/www/html/rr/levante-prova/
 echo resultats en http://www.riuraueditors.cat/levante-prova/levante.txt-lt.html
--- do-it.sh
+++ do-it.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mv levante.txt-lt.html levante.txt-lt-old.html
 ./clone.sh
 ./extract-html-files.sh
 ./analyze.sh
